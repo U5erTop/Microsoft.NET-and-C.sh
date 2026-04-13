c2338168ae6da593c977dda90f3d83c273f59fa7
U5erTop <prosvet751@gmail.com>
2026-04-13 10:47:30 +0300

    Add files via upload

---
+++ 14.04/Пример.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            Console.WriteLine($"a) = {x >=0 && (Math.Pow(y,2) != 4)}");
+
+            Console.WriteLine($"б) = {(x * y != 0) || (y > x)}");
+        }
+    }
+}
---
+++ 15.04/Task_14.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите X (|X| < 1): ");
+            double X = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите N (> 0): ");
+            int N = int.Parse(Console.ReadLine());
+
+            double sum = 0.0;
+            for (int k = 1; k <= N; k++)
+            {
+                double term = Math.Pow(-1, k + 1) * Math.Pow(X, k) / k;
+                sum += term;
+            }
+
+            Console.WriteLine($"Приближённое значение ln(1+X): {sum}");
+            Console.WriteLine($"Точное значение ln(1+X): {Math.Log(1 + X)}");
+
+            Console.ReadKey();
+        }
+    }
+}
